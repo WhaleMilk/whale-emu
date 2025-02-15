5e503ae233544c8c81b759fefe51efde77e0bdcc
WhaleMilk <collinmbarnett@gmail.com>
2025-02-14 22:10:43 -0700

    notes and stuff added

---
+++ src/core/cpu.rs
@@ -0,0 +1,9 @@
+/*
+ * What needs to happen:
+ * CPU needs initialize itself with PC to 0
+ * CPU creates a register page
+ * CPU creates an op code variable
+ * CPU creates variable that tracks step
+ * starts waiting for call to tick
+ * Tick function 
+*/
